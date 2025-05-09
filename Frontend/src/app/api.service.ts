d07ec44db8f647eccdb8b471569a12824f3c9297
i220957Ayishah <114764100+i220957Ayishah@users.noreply.github.com>
2025-05-09 16:29:53 +0500

    Docs: Update api.service.ts

--- Frontend/src/app/api.service.ts
+++ Frontend/src/app/api.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment'; // <--- Add this line
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable (
+  {
+    providedIn: 'root'
+  }
+)
 export class ApiService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+
+  }
 
   getPosts(): Observable<any> {
     return this.http.get(${environment.apiUrl}/posts);
